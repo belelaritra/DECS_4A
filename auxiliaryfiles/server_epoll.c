d5a6de6fa11ab196e4b1f37be8027bb29c0d31ce
belelaritra <belelaritra@gmail.com>
2024-10-21 20:34:17 +0530

    Q3 Updated

--- auxiliaryfiles/server_epoll.c
+++ auxiliaryfiles/server_epoll.c
@@ -8,121 +8,114 @@
 #include <sys/epoll.h>
 
 #define BUF_SIZE 1024
+#define MAX_EVENTS 10
 #define MAX_CLIENTS 10
-#define PORT 8080
 
-int main() {
-    int sock, new_sock, epoll_fd, event_count;
-    struct sockaddr_in address;
-    char *message = "world";
-    char buffer[BUF_SIZE];
-    
-    // Create socket
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Socket creation error");
+int main(int argc, char *argv[])
+{
+    if (argc != 2) {
+        printf("Usage: %s <port>\n", argv[0]);
         return -1;
     }
 
-    // Set up the address structure
+    int port = atoi(argv[1]);
+    int sock, new_sock, epfd, ret;
+    struct sockaddr_in address;
+    char *message = "world";
+    char buffer[BUF_SIZE] = {0};
+
+    struct epoll_event event, events[MAX_EVENTS];
+
+    // Create Socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        printf("Socket creation error\n");
+        return -1;
+    }
+
+    // Set address
     address.sin_family = AF_INET;
-    address.sin_port = htons(PORT);
+    address.sin_port = htons(port);
     address.sin_addr.s_addr = INADDR_ANY;
 
-    // Bind the socket
+    // Bind
     if (bind(sock, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("Bind failed");
+        printf("Bind Failed\n");
         close(sock);
         return -1;
     }
 
-    // Listen for incoming connections
-    if (listen(sock, 3) < 0) {
-        perror("Listen failed");
+    // Listen
+    if (listen(sock, MAX_CLIENTS) < 0) {
+        printf("Listen Failed\n");
         close(sock);
         return -1;
     }
 
     // Create epoll instance
-    if ((epoll_fd = epoll_create1(0)) < 0) {
-        perror("Epoll creation failed");
+    if ((epfd = epoll_create1(0)) < 0) {
+        printf("Epoll creation failed\n");
         close(sock);
         return -1;
     }
 
-    // Add the listening socket to the epoll instance
-    struct epoll_event event;
-    event.events = EPOLLIN;
+    // Add server socket to epoll
     event.data.fd = sock;
-    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, sock, &event) < 0) {
-        perror("Epoll_ctl failed");
+    event.events = EPOLLIN;
+    if (epoll_ctl(epfd, EPOLL_CTL_ADD, sock, &event) < 0) {
+        printf("Failed to add socket to epoll\n");
         close(sock);
-        close(epoll_fd);
         return -1;
     }
 
-    // Event loop
     while (1) {
-        struct epoll_event events[MAX_CLIENTS];
-
-        // Wait for events
-        event_count = epoll_wait(epoll_fd, events, MAX_CLIENTS, -1);
-        if (event_count < 0) {
-            perror("Epoll_wait failed");
+        int num_fds = epoll_wait(epfd, events, MAX_EVENTS, -1);
+        if (num_fds < 0) {
+            printf("Epoll wait error\n");
             break;
         }
 
-        // Iterate through the events
-        for (int i = 0; i < event_count; i++) {
-            // Check if it's the listening socket
-            if (events[i].data.fd == sock) {
+        for (int i = 0; i < num_fds; i++) {
+            // For Server
+            if (events[i].data.fd == sock) 
+            {
                 socklen_t addrlen = sizeof(address);
                 if ((new_sock = accept(sock, (struct sockaddr *)&address, &addrlen)) < 0) {
-                    perror("Accept failed");
+                    printf("Accept Failed\n");
                     continue;
                 }
 
-                // Add the new socket to the epoll instance
-                event.events = EPOLLIN;
+                // Add new clients
                 event.data.fd = new_sock;
-                if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, new_sock, &event) < 0) {
-                    perror("Epoll_ctl failed for new socket");
-                    close(new_sock);
-                    continue;
-                }
-            } else {
-                // Handle client data
-                new_sock = events[i].data.fd;
-                memset(buffer, 0, BUF_SIZE);
-                int ret = recv(new_sock, buffer, BUF_SIZE - 1, 0);
-                if (ret <= 0) {
-                    // Client disconnected or error occurred
-                    if (ret == 0) {
-                        printf("Client disconnected, socket fd: %d\n", new_sock);
-                    } else {
-                        perror("recv error");
-                    }
-                    close(new_sock);
-                    epoll_ctl(epoll_fd, EPOLL_CTL_DEL, new_sock, NULL);
-                } else {
-                    buffer[ret] = '\0'; // Null-terminate the buffer
+                event.events = EPOLLIN;
+                epoll_ctl(epfd, EPOLL_CTL_ADD, new_sock, &event);
+
+            } 
+            // For Clients
+            else 
+            {
+                int new_sock = events[i].data.fd;
+                ret = recv(new_sock, buffer, BUF_SIZE, 0);
+                // Client Disconnect
+                if (ret == 0) {
+                    printf("Client disconnected, socket fd: %d\n", new_sock);
+                } 
+                // Check and Response
+                else if (ret > 0) {
+                    buffer[ret] = '\0';
                     printf("Received: %s\n", buffer);
 
-                    // Respond if message is "hello"
                     if (strcmp(buffer, "hello") == 0) {
                         send(new_sock, message, strlen(message), 0);
                         printf("Sent: %s\n", message);
                     }
-
-                    // Close the connection after sending the response
-                    close(new_sock);
-                    epoll_ctl(epoll_fd, EPOLL_CTL_DEL, new_sock, NULL);
                 }
+                close(new_sock);
+                epoll_ctl(epfd, EPOLL_CTL_DEL, new_sock, NULL);
             }
         }
     }
 
-    // Clean up
     close(sock);
-    close(epoll_fd);
+    close(epfd);
     return 0;
 }
